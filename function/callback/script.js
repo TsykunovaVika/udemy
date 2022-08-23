78d4715409431d3856f67265f3db5905a8854b5f
Tsykunova Victoria <vika.tsykunova02@gmail.com>
2022-08-23 14:18:39 +0300

    1

--- function/callback/script.js
+++ function/callback/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+function first() {
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
+}
+
+
+function scecond(){
+    console.log(2);
+}
+
+first();
+scecond();
+
+function leatnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+leatnJS('JavaScript', function(){
+    console.log('Я прошёл это урок')
+})
